3544a6483270a8aa9c1ffd27a4b7f71d9469b52d
Misha86 <mpolishchuk86@gmail.com>
2022-05-01 13:50:09 +0300

    added database settings for heroku

--- beauty/settings.py
+++ beauty/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+
+import dj_database_url
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,12 +77,10 @@ WSGI_APPLICATION = 'beauty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': dj_database_url.config(conn_max_age=500, default='postgres://postgres:postgres@localhost:5432/beauty')}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
