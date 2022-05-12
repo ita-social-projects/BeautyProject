af1b513842940c32b60fbf35d5106d0948a5495a
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-12 19:16:41 +0300

    fixed nginx install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,4 +6,9 @@ sudo yum install gcc libc-dev linux-headers postgresql-dev postgresql-client
 
 sudo pip3 install -r requirements.txt 
 
-sudo yum install -y nginx
+sudo amazon-linux-extras list | grep nginx
+
+sudo amazon-linux-extras enable nginx1
+        
+sudo yum clean metadata
+sudo yum -y install nginx
