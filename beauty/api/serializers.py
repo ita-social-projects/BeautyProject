3863f7d0655eda26b55d5e6f62e71a27f92e848e
Misha86 <mpolishchuk86@gmail.com>
2022-05-12 09:41:05 +0300

    #83:Added groups choose and confirm password field for Post method user creation form

--- beauty/api/serializers.py
+++ beauty/api/serializers.py
@@ -1,55 +1,61 @@
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.reverse import reverse
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import Group
 
 from api.models import CustomUser
 
 
-# class CustomerHyperlink(serializers.HyperlinkedRelatedField):
-#     view_name = 'authentication:user-order-detail'
-#
-#     def get_url(self, obj, view_name, request, format):
-#         url_kwargs = {
-#             'user_id': obj.user.pk,
-#             'id': obj.pk
-#         }
-#         return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
-#
-#     def get_object(self, view_name, view_args, view_kwargs):
-#         lookup_kwargs = {
-#             'user_id': view_kwargs['user_id'],
-#             'id': view_kwargs['id']
-#         }
-#         return self.get_queryset().get(**lookup_kwargs)
+class PasswordsValidation(serializers.Serializer):
+
+    def validate(self, data):
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+        if password and confirm_password:
+            if password != confirm_password:
+                raise serializers.ValidationError({"password": "Password confirmation does not match."})
+        elif any([password, confirm_password]):
+            raise serializers.ValidationError({"confirm_password": "Didn`t enter the password confirmation."})
+
+        return super().validate(data)
 
 
 class GroupListingField(serializers.RelatedField):
+
     def to_representation(self, value):
         return value.name
 
+    def to_internal_value(self, data):
+        return self.get_queryset().get(name=data).id
 
-class CustomUserSerializer(serializers.HyperlinkedModelSerializer):
+
+class CustomUserSerializer(PasswordsValidation, serializers.HyperlinkedModelSerializer):
+    queryset = Group.objects.all()
+
     url = serializers.HyperlinkedIdentityField(view_name='api:user-detail', lookup_field='pk')
 
-    password = serializers.CharField(write_only=True, required=True, help_text='Leave empty if no change needed',
+    password = serializers.CharField(write_only=True, required=True,
                                      style={'input_type': 'password', 'placeholder': 'Password'},
                                      validators=[validate_password])
-    groups = GroupListingField(many=True, read_only=True)
+    confirm_password = serializers.CharField(write_only=True, required=True,
+                                             style={'input_type': 'password',
+                                                    'placeholder': 'Confirmation Password'})
+    groups = GroupListingField(many=True, required=False, queryset=queryset)
 
     class Meta:
         model = CustomUser
         fields = ['url', 'id', 'email', 'first_name', 'patronymic', 'last_name',
-                  'phone_number', 'bio', 'rating', 'avatar', 'groups', 'is_active', 'password']
+                  'phone_number', 'bio', 'rating', 'avatar', 'groups', 'is_active', 'password', 'confirm_password']
 
     def create(self, validated_data):
+        validated_data.pop('confirm_password')
         user = CustomUser.objects.create_user(**validated_data)
         return user
 
 
-class CustomUserDetailSerializer(serializers.ModelSerializer):
+class CustomUserDetailSerializer(PasswordsValidation, serializers.ModelSerializer):
     # orders = CustomerHyperlink(many=True, read_only=True)
-    groups = GroupListingField(many=True, read_only=True)
+    groups = GroupListingField(many=True, queryset=Group.objects.all())
     password = serializers.CharField(write_only=True, allow_blank=True, validators=[validate_password],
                                      style={'input_type': 'password', 'placeholder': 'New Password'})
 
@@ -63,36 +69,8 @@ class CustomUserDetailSerializer(serializers.ModelSerializer):
         fields = ['id', 'email', 'first_name', 'patronymic', 'last_name',
                   'phone_number', 'bio', 'rating', 'avatar', 'groups', 'is_active', 'password', 'confirm_password']
 
-    def validate(self, data):
-        password = data.get('password')
-        confirm_password = data.get('confirm_password')
-        if password and confirm_password:
-            if password != confirm_password:
-                raise serializers.ValidationError({"password": "Password confirmation does not match."})
-        elif any([password, confirm_password]):
-            raise serializers.ValidationError({"confirm_password": "Didn`t enter the password confirmation."})
-
-        return super().validate(data)
-
     def update(self, instance, validated_data):
         confirm_password = validated_data.pop('confirm_password')
         if confirm_password:
             validated_data['password'] = make_password(confirm_password)
         return super().update(instance, validated_data)
-
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     groups_name = Group.objects.filter(id__in=representation['groups']).values_list("name", flat=True)
-    #     representation['groups'] = groups_name
-    #     return representation
-
-
-# class UserOrderDetailSerializer(serializers.HyperlinkedModelSerializer):
-#     book = serializers.SlugRelatedField(read_only=True, slug_field='name')
-#     user = serializers.CharField(source='user.get_full_name')
-#
-#     url = serializers.HyperlinkedIdentityField(view_name='order:order-detail', lookup_field='pk')
-#
-#     class Meta:
-#         model = Order
-#         fields = ['url', 'id', 'user', 'book', 'created_at', 'end_at', 'plated_end_at']
