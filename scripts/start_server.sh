4fc762060828554662b28e9d8f2166d6503183f8
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-12 23:47:31 +0300

    start app

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -20,6 +20,6 @@ python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput
 
-python3.9 manage.py 0.0.0.0:8000
+python3.9 manage.py runserver 0.0.0.0:8000
 
 # gunicorn beauty.wsgi:application --bind 0.0.0.0:8000
