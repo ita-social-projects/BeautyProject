0f1f194d98a68e0ef5f3feca6c8ba166ac14fbaa
Dmytro Tymoshchenko <dietrich.kreze@gmail.com>
2022-05-12 19:34:50 +0300

    Added Order model

--- beauty/api/models.py
+++ beauty/api/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import PermissionsMixin
 from django.core.validators import validate_email
 from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 def upload_location(instance, filename):
@@ -105,3 +106,150 @@ class CustomUser(PermissionsMixin, AbstractBaseUser):
         :return: class, id
         """
         return f'{self.__class__.__name__}(id={self.id})'
+
+
+class Order(models.Model):
+    """This class represents a basic Order (for an appointment system)
+    that stores all the required information.
+
+    Note:
+        reason attribute is only neaded if order's status is cancelled
+        end_time is autocalculated during creation, no need to put it
+
+    Attributes:
+        status: Status of the order
+        start_time: Appointment time and date of the order
+        end_time: Time that is calculated according to the duration of service
+        created_at: Time of creation of the order
+        specialist: An appointed specialist for the order
+        customer: A customer who will recieve the order
+        service: Service that will be fulfield for the order
+        reason: (optional) Reason for cancellation
+    
+    Properties:
+        is_active: Returns true if order's status is active
+        is_approved: Returns true if order's status is approved
+        is_declined: Returns true if order's status is declined
+
+    """
+
+    class StatusChoices(models.TextChoices):
+        """This class is used for status codes"""
+        ACTIVE = 0, _('Active')
+        COMPLETED = 1, _('Completed')
+        CANCELLED = 2, _('Cancelled')
+        APPROVED = 3, _('Approved')
+        DECLINED = 4, _('Declined')
+
+    class Meta:
+        """This meta class stores ordering and permissions data"""
+        ordering = ['id']
+        unique_together = ['specialist', 'customer']
+        get_latest_by = "created_at"
+        permissions = [
+        ('can_add_order', 'Can add an order'),
+        ('can_change_order', 'Can change an order'),
+        ('can_set_status', 'Can set a status of the order'),
+        ('can_view_order', 'Can view an order')
+        ]
+    
+    status = models.CharField(
+        max_length=2,
+        choices=StatusChoices.choices,
+        default=StatusChoices.ACTIVE,
+        verbose_name=_('Current status')
+    )
+    start_time = models.DateTimeField(
+        editable=True, 
+        verbose_name=_('Appointment time')
+    )
+    end_time = models.DateTimeField(
+        editable=False, 
+        verbose_name=_('End time')
+    )
+    created_at = models.DateTimeField(
+        auto_now=True, 
+        editable=False, 
+        verbose_name=_('Created at')
+    )
+    specialist = models.ForeignKey(
+        'CustomUser',
+        related_name = 'specialist_user', 
+        on_delete=models.CASCADE,
+        verbose_name=_('Specialist')
+    )
+    customer = models.ForeignKey(
+        'CustomUser', 
+        related_name = 'customer_user',
+        on_delete=models.CASCADE,
+        verbose_name=_('Customer')
+        )
+    service = models.ForeignKey(
+        'Service', 
+        on_delete=models.CASCADE,
+        verbose_name=_('Service')
+    )
+    reason = models.TextField(
+        blank=True, 
+        null=True,
+        verbose_name=_('Reason for cancellation')
+    )
+
+    def save(self, *args, **kwargs):
+        """Reimplemented save method for end_time calculation"""
+        from datetime import timedelta
+        self.end_time = self.start_time + timedelta(minutes=self.service.duration)
+        super(Order, self).save(*args, **kwargs)
+        return self
+
+    @property
+    def is_active(self) -> bool:
+        """bool: Returns true if order's status is active"""
+        return self.status == self.StatusChoices.ACTIVE
+    
+    @property
+    def is_approved(self) -> bool:
+        """bool: Returns true if order's status is approved"""
+        return self.status == self.StatusChoices.APPROVED
+
+    @property
+    def is_declined(self) -> bool:
+        """bool: Returns true if order's status is declined"""
+        return self.status == self.StatusChoices.DECLINED
+
+    def mark_as_approved(self):
+        """Marks order as approved"""
+        self.status = self.StatusChoices.APPROVED
+        self.save(update_fields=['status'])
+
+    def mark_as_cancelled(self):
+        """Marks order as cancelled"""
+        self.status = self.StatusChoices.CANCELLED
+        self.save(update_fields=['status'])
+
+    def mark_as_completed(self):
+        """Marks order as completed"""
+        self.status = self.StatusChoices.COMPLETED
+        self.save(update_fields=['status'])
+
+    def mark_as_declined(self):
+        """Marks order as declined"""
+        self.status = self.StatusChoices.DECLINED
+        self.save(update_fields=['status'])
+
+    def add_reason(self, reason: str):
+        """Add a reason for an order"""
+        self.reason = reason
+        self.save(update_fields=['reason'])
+    
+    def get_reason(self) -> str:
+        """str: Returns a reason"""
+        return self.reason
+    
+    def __str__(self) -> str:
+        """str: Returns a verbose title of the order"""
+        return f"Order #{self.id}"
+
+    def __repr__(self) -> str:
+        """str: Returns a string representation of the order"""
+        return f"Order #{self.id} ({self.status})"
