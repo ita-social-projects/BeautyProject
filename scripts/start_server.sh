c753ea267e205356189e468af0e2fe0fce7375b2
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-12 22:34:55 +0300

    changed to python3

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -17,8 +17,8 @@ pwd
 
 ls
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 
 gunicorn beauty.wsgi:application --bind 0.0.0.0:8000
