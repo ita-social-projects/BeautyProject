a80ad8880aa2d5fd5a02041950685334f007db07
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-12 22:36:45 +0300

    changed installing dependecies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,8 +4,6 @@ sudo yum update -y
 
 sudo yum install gcc libc-dev linux-headers postgresql-dev postgresql-client
 
-sudo pip3 install -r requirements.txt 
-
 sudo amazon-linux-extras list | grep nginx
 
 sudo amazon-linux-extras enable nginx1
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -17,6 +17,8 @@ pwd
 
 ls
 
+sudo pip3 install -r requirements.txt
+
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
