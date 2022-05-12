7c4daa4c52d7da9e7aff88a00d563b2674dc7a16
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-12 23:26:14 +0300

    added source for psycopg2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,9 +2,13 @@
 
 sudo yum update -y
 
-sudo yum install gcc libc-dev linux-headers postgresql-dev \ 
-                 postgresql-client libpq-dev python3-dev \
-                 python-setuptools python-devel postgresql-devel
+# sudo yum install gcc libc-dev linux-headers postgresql-dev \ 
+#                  postgresql-client libpq-dev python3-dev \
+#                  python-setuptools python-devel postgresql-devel
+
+yum install -y python-psycopg2 postgresql libncurses5-dev libffi libffi-devel libxml2-devel libxslt-devel libxslt1-dev
+yum install -y postgresql-libs postgresql-devel python-lxml python-devel gcc patch python-setuptools
+yum install -y gcc-c++ flex epel-release nginx supervisor
 
 sudo amazon-linux-extras list | grep nginx
 
