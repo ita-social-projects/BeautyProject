a8eefdd5e7be69414f164ed1139aaf76862569b1
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-12 23:28:11 +0300

    start app

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,10 +2,6 @@
 
 sudo yum update -y
 
-# sudo yum install gcc libc-dev linux-headers postgresql-dev \ 
-#                  postgresql-client libpq-dev python3-dev \
-#                  python-setuptools python-devel postgresql-devel
-
 yum install -y python-psycopg2 postgresql libncurses5-dev libffi libffi-devel libxml2-devel libxslt-devel libxslt1-dev
 yum install -y postgresql-libs postgresql-devel python-lxml python-devel gcc patch python-setuptools
 yum install -y gcc-c++ flex epel-release nginx supervisor
