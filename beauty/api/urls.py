6866e20b0f6e633bf53ecb29a23ccdafc64f3431
Сергій Волошин <serhii.voloshyn59@gmail.com>
2022-05-13 15:17:07 +0300

    Urls and View changes

--- beauty/api/urls.py
+++ beauty/api/urls.py
@@ -6,6 +6,8 @@ from .serializers import CustomUserSerializer
 from rest_framework.generics import ListCreateAPIView
 from .views import CustomUserGenerics, CustomUserDetailGenerics
 
+app_name = "api"
+
 urlpatterns = [
     path('', CustomUserGenerics.as_view(), name='user-list'),
     path('<int:pk>/', CustomUserDetailGenerics.as_view(), name='user-detail'),
