ce6b73a80143d4cfba548e5c63da96e229651047
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-13 11:28:31 +0300

    idk what i do

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cd /home/ec2-user/Beauty/beauty
+cd /home/ec2-user/Beauty
 
 python3.9 -m venv venv
 
 source venv/bin/activate
 
 pip3.9 install -r requirements.txt
+
+cd beauty
 
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
