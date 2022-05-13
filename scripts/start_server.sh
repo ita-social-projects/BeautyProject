1efc214638b412339675a84a617283f830e9fb3e
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-13 11:33:16 +0300

    idk what i do

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -12,6 +12,8 @@ cd beauty
 
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput && gunicorn beauty.wsgi:application --bind 0.0.0.0:8000
+python3.9 manage.py collectstatic --noinput
+
+gunicorn beauty.wsgi:application --bind 0.0.0.0:8000 --daemon
 
 # sudo nginx -c /etc/nginx/nginx.conf
