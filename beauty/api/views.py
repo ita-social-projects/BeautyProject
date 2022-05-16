3087b96fc852319c99e6aed81db3b1f5b030d218
therobotisnotatoy <pythonshumor@gmail.com>
2022-05-16 18:48:52 +0300

    #83 #63 : Combine two different branches to commit into a dev_model branch

--- beauty/api/views.py
+++ beauty/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from .models import CustomUser
+from .serializers.serializers_customuser import CustomUserDetailSerializer
+from .serializers.serializers_customuser import CustomUserSerializer
+
+
+class CustomUserListCreateView(ListCreateAPIView):
+    """Generic API for custom POST method"""
+
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+    # Permissions
+    # line with max chars in code
+
+
+class CustomUserDetailRUDView(RetrieveUpdateDestroyAPIView):
+    """Generic API for custom GET, PUT and DELETE method.
+    RUD - Retrieve, Update, Destroy"""
+
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserDetailSerializer
+
+    # Permissions
+    # rest git hub
