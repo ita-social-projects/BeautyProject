32c1b730e1d7d2b583b6d70ffbb9348263e63521
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-17 10:31:20 +0300

    removed useless scripts I accidently pushed

--- scripts/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo yum update -y
-
-yum install -y python-psycopg2 postgresql libncurses5-dev libffi libffi-devel libxml2-devel libxslt-devel libxslt1-dev
-yum install -y postgresql-libs postgresql-devel python-lxml python-devel gcc patch python-setuptools
-yum install -y gcc-c++ flex epel-release nginx supervisor
-
-sudo amazon-linux-extras install nginx1
--- scripts/stop_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-if [ -e /var/run/nginx.pid ]; then sudo nginx -s stop; fi
