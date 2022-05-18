1fa241ea0a70178b314c17afbc6ee8ce3e2d7ba0
Сергій Волошин <serhii.voloshyn59@gmail.com>
2022-05-18 11:10:25 +0300

    #63: Finished tests

--- beauty/api/tests/test_registration_POST_method.py
+++ beauty/api/tests/test_registration_POST_method.py
@@ -1,8 +1,7 @@
-import pytest
+"""Module contains test for generic post method for user registration"""
+
 from django.test import TestCase
 from rest_framework.test import APIClient
-from rest_framework import status
-import unittest
 
 from api.serializers.serializers_customuser import (
                                                     CustomUserSerializer,
@@ -13,10 +12,18 @@ from api.views import CustomUserListCreateView
 
 
 class TestCustomUserListCreateView(TestCase):
+    """TestCase to test POST method, which is generics.ListCreateView"""
+
     def setUp(self):
         """
         Create 3 CustomUser instances.
         """
+        # List of 3 dicts, where each dict contains CustomUser data
+        # 1 element correct,
+        # 2 element with invalid phone,
+        # 3 with invalid confirm_password
+        # -1 and -2 elements are valid,
+        # and used for creating objects for testing GET method
         self.items = [
                 {
                     "url": "http://testserver/api/v1/user/10/",
@@ -29,7 +36,7 @@ class TestCustomUserListCreateView(TestCase):
                     "bio": "Specialist_6",
                     "rating": 0,
                     "is_active": True,
-                    "groups": ['Specialist', ],
+                    "groups": [],
                     'specialist_orders': [],
                     'customer_orders': [],
                     "password": "0967478911m",
@@ -42,17 +49,16 @@ class TestCustomUserListCreateView(TestCase):
                     "first_name": "Specialist_6",
                     "patronymic": "PSpecialist_6",
                     "last_name": "LSpecialist_6",
-                    "phone_number": "+380967470017",
+                    "phone_number": "+38096747001",
                     "bio": "Specialist_6",
                     "rating": 0,
                     "is_active": True,
-                    "groups": ['Specialist', ],
+                    "groups": [],
                     'specialist_orders': [],
                     'customer_orders': [],
                     "password": "0967478911m",
                     "confirm_password": "0967478911m"
                 },
-                
                 {
                     "url": "http://testserver/api/v1/user/12/",
                     "id": 12,
@@ -64,7 +70,41 @@ class TestCustomUserListCreateView(TestCase):
                     "bio": "Specialist_6",
                     "rating": 0,
                     "is_active": True,
-                    "groups": ['Specialist', ],
+                    "groups": [],
+                    'specialist_orders': [],
+                    'customer_orders': [],
+                    "password": "0967478911m",
+                    "confirm_password": "0967478911"
+                },
+                {
+                    "url": "http://testserver/api/v1/user/10/",
+                    "id": 15,
+                    "email": "m611@com.ua",
+                    "first_name": "Specialist_6",
+                    "patronymic": "PSpecialist_6",
+                    "last_name": "LSpecialist_6",
+                    "phone_number": "+380967470019",
+                    "bio": "Specialist_6",
+                    "rating": 0,
+                    "is_active": True,
+                    "groups": [],
+                    'specialist_orders': [],
+                    'customer_orders': [],
+                    "password": "0967478911m",
+                    "confirm_password": "0967478911m"
+                },
+                {
+                    "url": "http://testserver/api/v1/user/10/",
+                    "id": 16,
+                    "email": "m6111@com.ua",
+                    "first_name": "Specialist_6",
+                    "patronymic": "PSpecialist_6",
+                    "last_name": "LSpecialist_6",
+                    "phone_number": "+380967470020",
+                    "bio": "Specialist_6",
+                    "rating": 0,
+                    "is_active": True,
+                    "groups": [],
                     'specialist_orders': [],
                     'customer_orders': [],
                     "password": "0967478911m",
@@ -76,25 +116,79 @@ class TestCustomUserListCreateView(TestCase):
         self.queryset = CustomUser.objects.all()
         self.view = CustomUserListCreateView.as_view()
         self.client = APIClient()
-        
+
+        # Needed for GET method testing
+        self.client.post(
+                        'http://127.0.0.1:8000/api/v1/user/',
+                        data=self.items[-1]
+                        )
+        self.client.post(
+                        'http://127.0.0.1:8000/api/v1/user/',
+                        data=self.items[-2]
+                        )
+
 
     def test_get_custom_user_list_create_view(self):
         """
         GET requests to ListCreateAPIView should return list of objects.
         """
-        response = self.client.get('http://127.0.0.1:8000/api/v1/user/')
-        assert response.status_code == status.HTTP_200_OK
-
+        # self.client.generic needed for getting data from GET method
+        resp = self.client.generic(
+                                    method="GET",
+                                    path="http://127.0.0.1:8000/api/v1/user/"
+                                    )
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.data), 2)
 
     def test_post_custom_user_list_create_view(self):
         """
         POST requests to ListCreateAPIView should create a new object.
         """
-        response = self.client.post('http://127.0.0.1:8000/api/v1/user/', data=self.items[0])
-        assert response.status_code == status.HTTP_201_CREATED
+        response = self.client.post(
+                                    'http://127.0.0.1:8000/api/v1/user/',
+                                    data=self.items[0]
+                                    )
+        self.assertEqual(response.status_code, 201)
 
-    def test_post_cannot_set_id(self):
+    def test_post_dublicate_custom_user(self):
         """
-        POST requests to create a new object should not be able to set the id.
+        POST requests to ListCreateAPIView
+        should not create a new object if same object already exists.
         """
-        pass
+        response = self.client.post(
+                                    'http://127.0.0.1:8000/api/v1/user/',
+                                    data=self.items[-1]
+                                    )
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_invalid_phone(self):
+        """
+        POST requests to ListCreateAPIView with phone number which doesn't match regex
+        should not create a new object.
+        """
+        response = self.client.post(
+                                    'http://127.0.0.1:8000/api/v1/user/',
+                                    data=self.items[1]
+                                    )
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_invalid_confirm_password(self):
+        """
+        POST requests to ListCreateAPIView
+        with different password and confirm_password should not create a new object.
+        """
+        response = self.client.post(
+                                    'http://127.0.0.1:8000/api/v1/user/',
+                                    data=self.items[2]
+                                    )
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_empty_data(self):
+        """
+        POST requests to ListCreateAPIView with empty data shoul not create a new object.
+        """
+        response = self.client.post(
+                                    'http://127.0.0.1:8000/api/v1/user/',
+                                    data={}
+                                    )
+        self.assertEqual(response.status_code, 400)
