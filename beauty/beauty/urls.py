cd3f2406ca311ac3e50a56d1eda329b3fc65643b
Dmytro Tymoshchenko <dietrich.kreze@gmail.com>
2022-05-18 21:00:54 +0300

    Fixed emails for deploy

--- beauty/beauty/urls.py
+++ beauty/beauty/urls.py
@@ -47,10 +47,10 @@ urlpatterns = [
     # path('api/v1/auth/', include('djoser.urls')),
     # path('api/v1/auth_token/', include('djoser.urls.authtoken')),
     path('api/v1/user/', include('api.urls', namespace="api")),
-    path(
-        'api-auth/',
-     include('rest_framework.urls', namespace='rest_framework')
-     ),
+    #path(
+    #    'api-auth/',
+    # include('rest_framework.urls', namespace='rest_framework')
+    # ),
     path(r'auth/', include('djoser.urls')),
     path(r'auth/', include('djoser.urls.jwt'))
 
