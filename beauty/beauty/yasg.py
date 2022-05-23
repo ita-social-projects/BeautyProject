60df89da6424d0f265adffd2ba0e3fc206545587
Misha86 <mpolishchuk86@gmail.com>
2022-05-23 21:23:55 +0300

    #144:Changed yasg.py file

--- beauty/beauty/yasg.py
+++ beauty/beauty/yasg.py
@@ -1,24 +1,26 @@
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Beauty Project",
-      default_version='v1',
-      description="The best site",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny, ),
+    openapi.Info(
+        title="Beauty Project",
+        default_version='v1',
+        description="The best site",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    # public=True,
+    public=False,
+    # permission_classes=(permissions.AllowAny,),
+    permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
 )
 
 urlpatterns = [
-    path('swagger(<format>\.json|\.yaml)',
-         schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
