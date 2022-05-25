f5ab25d7a3e27021af56de9bc873a32bbc0545dc
Misha86 <mpolishchuk86@gmail.com>
2022-05-26 01:36:52 +0300

    #140: Added query for specialist field

--- beauty/api/serializers/order_serializers.py
+++ beauty/api/serializers/order_serializers.py
@@ -1,7 +1,16 @@
 """The module includes serializers for Order model."""
 
 from rest_framework import serializers
-from api.models import Order
+from api.models import (Order, CustomUser, Service)
+
+
+class SpecialistRelatedField(serializers.PrimaryKeyRelatedField):
+    """RelatedField for choose specialist in the POST method."""
+
+    def get_queryset(self):
+        """Get query with all users which are specialists."""
+        return self.queryset.filter(
+                groups__name__icontains="specialist")
 
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
@@ -10,9 +19,11 @@ class OrderSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='api:order-detail', lookup_field='pk'
     )
-    specialist = serializers.PrimaryKeyRelatedField(read_only=True)
-    customer = serializers.PrimaryKeyRelatedField(read_only=True)
-    service = serializers.PrimaryKeyRelatedField(read_only=True)
+    specialist = SpecialistRelatedField(queryset=CustomUser.objects.all())
+    customer = serializers.PrimaryKeyRelatedField(
+        queryset=CustomUser.objects.all())
+    service = serializers.PrimaryKeyRelatedField(
+        queryset=Service.objects.all())
 
     class Meta:
         """Class with a model and model fields for serialization."""
