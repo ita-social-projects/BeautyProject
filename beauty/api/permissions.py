970c7889fd1dd107272354d0c024783a69629f0e
Misha86 <mpolishchuk86@gmail.com>
2022-05-27 21:40:44 +0300

    #167: Changed permissions.py

--- beauty/api/permissions.py
+++ beauty/api/permissions.py
@@ -18,32 +18,17 @@ class IsAdminOrIsAccountOwnerOrReadOnly(permissions.BasePermission):
     or admin to edit it.
     """
 
-    def has_permission(self, request, view):
-        """Read permissions are allowed to any request,
-        so we'll always allow GET, HEAD or OPTIONS requests.
-        """
-        return bool(request.method in permissions.SAFE_METHODS)
-
     def has_object_permission(self, request, view, obj):
         if request.user.is_authenticated:
-            if request.user.is_admin or (obj.email == request.user.email):
+            if request.user.is_admin or (obj == request.user):
                 return True
-        return False
+        return bool(request.method in permissions.SAFE_METHODS)
 
 
 class IsAccountOwnerOrReadOnly(permissions.BasePermission):
     """Object-level permission to only allow owners of an object to edit it."""
 
-    def has_permission(self, request, view):
-        """Read permissions are allowed to any request,
-        so we'll always allow GET, HEAD or OPTIONS requests.
-        """
-        return bool(
-            request.method in permissions.SAFE_METHODS or
-            request.user.is_authenticated
-        )
-
     def has_object_permission(self, request, view, obj):
         if request.user.is_authenticated:
-            return bool(obj.email == request.user.email)
-        return False
+            return bool(obj == request.user)
+        return bool(request.method in permissions.SAFE_METHODS)
