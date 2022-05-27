6bf0404ca6b80d17f5336d7cf924cfd9b9d8bf96
Dmytro Tymoshchenko <dietrich.kreze@gmail.com>
2022-05-28 00:18:11 +0300

    Fixed link issues, merged new items from dev

--- beauty/api/views.py
+++ beauty/api/views.py
@@ -31,6 +31,7 @@ from .serializers.review_serializers import ReviewAddSerializer
 
 logger = logging.getLogger(__name__)
 
+logger = logging.getLogger(__name__)
 
 class CustomUserListCreateView(ListCreateAPIView):
     """Generic API for users custom POST methods."""
