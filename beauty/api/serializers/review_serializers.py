05fcde8517dd1f8840199add696e44852a12240b
Oleksii Datsiuk <Olmine2013@gmail.com>
2022-05-30 10:31:10 +0300

    #145 : Created a serializer for displaying a review.

---
+++ beauty/api/serializers/review_serializers.py
@@ -0,0 +1,22 @@
+"""The module includes serializers for Review model."""
+
+from rest_framework import serializers
+from api.models import Review
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ReviewDisplaySerializer(serializers.ModelSerializer):
+    """This is a serializer for review display"""
+
+    class Meta:
+        """This is a class Meta that keeps settings for serializer"""
+
+        model = Review
+        fields = ("text_body",
+                  "rating",
+                  "from_user",
+                  "to_user",
+                  "date_of_publication")
