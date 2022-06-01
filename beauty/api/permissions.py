4da0a9511d3cf39fdbb51bf9318c668f5c55984d
therobotisnotatoy <pythonshumor@gmail.com>
2022-06-01 09:51:36 +0300

    #135: Added logging

--- beauty/api/permissions.py
+++ beauty/api/permissions.py
@@ -1,5 +1,10 @@
 """This module provides all needed permissions."""
+import logging
+
 from rest_framework import permissions
+
+
+logger = logging.getLogger(__name__)
 
 
 class IsAccountOwnerOrReadOnly(permissions.BasePermission):
@@ -9,10 +14,21 @@ class IsAccountOwnerOrReadOnly(permissions.BasePermission):
     to edit it.
     """
 
+    def has_permission(self, request, view):
+        """Read permission.
+
+        Read permissions are allowed to any request,
+        so we'll always allow GET, HEAD or OPTIONS requests.
+        """
+        return bool(request.method in permissions.SAFE_METHODS)
+
     def has_object_permission(self, request, view, obj):
         """Object permission check."""
-        return bool(request.method in permissions.SAFE_METHODS or
-                    obj == request.user)
+        logger.debug(f"Object {obj.id} permission check")
+
+        if request.user.is_authenticated:
+            if request.user.is_admin or (obj.email == request.user.email):
+                return True
 
 
 class IsAdminOrAccountOwnerOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
@@ -24,8 +40,10 @@ class IsAdminOrAccountOwnerOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
 
     def has_object_permission(self, request, view, obj):
         """Object permission check."""
-        return bool(request.method in permissions.SAFE_METHODS or
-                    request.user.is_admin or (obj == request.user))
+        logger.debug(f"Object {obj.id} permission check")
+
+        is_admin = request.user.is_admin
+        return bool(request.method in permissions.SAFE_METHODS or is_admin or (obj == request.user))
 
 
 class IsAdminOrBusinessOwner(permissions.IsAuthenticatedOrReadOnly):
@@ -37,8 +55,11 @@ class IsAdminOrBusinessOwner(permissions.IsAuthenticatedOrReadOnly):
 
     def has_object_permission(self, request, view, obj):
         """Object permission check."""
+        logger.debug(f"Object {obj.id} permission check")
+
         try:
             is_admin = request.user.is_admin
             return bool(is_admin or (obj == request.user))
         except AttributeError:
-            raise NotImplementedError({"code 404": 'not yet implemented content'})
+            logger.warning(f"User {request.user} has no permission to visit this page")
+            raise PermissionError("Cannot obtain object")
