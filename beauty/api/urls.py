017268b15a6283c74b9e3fbee769911df5984487
Dmytro Tymoshchenko <dietrich.kreze@gmail.com>
2022-06-03 15:54:07 +0300

    Fixed links

--- beauty/api/urls.py
+++ beauty/api/urls.py
@@ -1,22 +1,67 @@
+"""This module provides all needed api urls."""
+
 from django.urls import path
-from .views import *
+
+from .views import (AllOrOwnerBusinessesListCreateAPIView, BusinessDetailRetrieveAPIView,
+                    CustomUserDetailRUDView, CustomUserListCreateView,
+                    OrderApprovingView, OrderListCreateView, OrderRetrieveUpdateDestroyView,
+                    OwnerBusinessDetailRUDView, ReviewAddView)
+
 
 app_name = "api"
 
 urlpatterns = [
-    path('users/', CustomUserListCreateView.as_view(),
-         name='user-list-create'),
-    path('user/<int:pk>/', CustomUserDetailRUDView.as_view(),
-         name='user-detail'),
-    path('user/<int:user>/order/<int:pk>/',
-         OrderRetrieveUpdateDestroyView.as_view(),
-         name='user-order-detail'),
-
-    path('orders/', OrderListCreateView.as_view(),
-         name='order-list-create'),
-    path('order/<int:pk>/', OrderRetrieveUpdateDestroyView.as_view(),
-         name='order-detail'),
-    path('order/<str:uid>/<str:token>/<str:status>/',
-         OrderApprovingView.as_view(),
-         name='order-approving'),
+    path(
+        "users/",
+        CustomUserListCreateView.as_view(),
+        name="user-list-create",
+    ),
+    path(
+        "user/<int:pk>/",
+        CustomUserDetailRUDView.as_view(),
+        name="user-detail",
+    ),
+    path(
+        "user/<int:user>/order/<int:pk>/",
+        OrderRetrieveUpdateDestroyView.as_view(),
+        name="user-order-detail",
+    ),
+    path(
+        "orders/", OrderListCreateView.as_view(),
+        name="order-list-create",
+    ),
+    path(
+        "order/<int:pk>/",
+        OrderRetrieveUpdateDestroyView.as_view(),
+        name="order-detail",
+    ),
+    path(
+        "order/<str:uid>/<str:token>/<str:status>/",
+        OrderApprovingView.as_view(),
+        name="order-approving",
+    ),
+    path(
+        "businesses/",
+        AllOrOwnerBusinessesListCreateAPIView.as_view(),
+        name="businesses-list-create",
+    ),
+    path(
+        "businesses/<int:owner_id>/",
+        AllOrOwnerBusinessesListCreateAPIView.as_view(),
+        name="certain-owners-businesses-list",
+    ),
+    path(
+        "businesses/<int:owner_id>/<int:pk>/",
+        OwnerBusinessDetailRUDView.as_view(),
+        name="owner-business-detail",
+    ),
+    path(
+        "business/<int:pk>/",
+        BusinessDetailRetrieveAPIView.as_view(),
+        name="business-detail",
+    ),
+    path(
+        r"<int:user>/reviews/add/",
+        ReviewAddView.as_view(),
+        name="review-add"),
 ]
