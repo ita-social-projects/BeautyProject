219781efd99d1eeba7c4a64234b981689d264ab8
Dmytro Tymoshchenko <dietrich.kreze@gmail.com>
2022-06-06 18:11:19 +0300

    Added check on nonspecialist users, added new test.

--- beauty/api/tests/test_POST_method_to_add_review.py
+++ beauty/api/tests/test_POST_method_to_add_review.py
@@ -3,6 +3,7 @@
 It tests ReviewAddView and ReviewAddSerializer.
 
 Tests:
+    *   Test that user can't review a non specialist
     *   Test that unauthorized user won't be able to access API
     *   Test that an authorized user can create a Review
     *   Test that serializer doesn't allow rating < 0
@@ -16,9 +17,11 @@ Tests:
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
-from .factories import CustomUserFactory, ReviewFactory
 from api.serializers.review_serializers import ReviewAddSerializer
-from api.views_api import ReviewAddView
+from api.views import ReviewAddView
+from .factories import (CustomUserFactory,
+                        ReviewFactory,
+                        GroupFactory)
 
 
 class TestReviewAddView(TestCase):
@@ -26,20 +29,32 @@ class TestReviewAddView(TestCase):
 
     def setUp(self) -> None:
         """This method adds needed info for tests."""
-        self.user = CustomUserFactory.create(is_active=True)
-        self.responder = CustomUserFactory.create(is_active=True)
+        self.groups = GroupFactory.groups_for_test()
+        self.reviewer = CustomUserFactory.create()
+        self.reviewee = CustomUserFactory.create()
+        self.nonspecialist = CustomUserFactory.create()
+        self.groups.specialist.user_set.add(self.reviewee)
 
         self.serializer = ReviewAddSerializer
         self.view = ReviewAddView
         self.client = APIClient()
-        self.client.force_authenticate(user=self.user)
+        self.client.force_authenticate(user=self.reviewer)
+
+    def test_post_method_create_review_non_specialist(self):
+        """A logged user can't review a nonspecialist."""
+        review = ReviewFactory()
+        response = self.client.post(
+            path=reverse("api:review-add", kwargs={"user": self.nonspecialist.id}),
+            data={"text_body": review.text_body, "rating": review.rating},
+        )
+        self.assertEqual(response.status_code, 400)
 
     def test_post_method_create_review_not_logged_user(self):
         """Only a logged user can create a review."""
         self.client.force_authenticate(user=None)
         review = ReviewFactory()
         response = self.client.post(
-            path=reverse("api:review-add", kwargs={"user": self.responder.id}),
+            path=reverse("api:review-add", kwargs={"user": self.reviewee.id}),
             data={"text_body": review.text_body, "rating": review.rating},
         )
         self.assertEqual(response.status_code, 401)
@@ -48,7 +63,7 @@ class TestReviewAddView(TestCase):
         """A logged user should be able to create a review."""
         review = ReviewFactory()
         response = self.client.post(
-            path=reverse("api:review-add", kwargs={"user": self.responder.id}),
+            path=reverse("api:review-add", kwargs={"user": self.reviewee.id}),
             data={"text_body": review.text_body, "rating": review.rating},
         )
         self.assertEqual(response.status_code, 201)
@@ -57,7 +72,7 @@ class TestReviewAddView(TestCase):
         """Rating should not be less than 0."""
         review = ReviewFactory(rating=-5)
         response = self.client.post(
-            path=reverse("api:review-add", kwargs={"user": self.responder.id}),
+            path=reverse("api:review-add", kwargs={"user": self.reviewee.id}),
             data={"text_body": review.text_body, "rating": review.rating},
         )
         self.assertEqual(response.status_code, 400)
@@ -66,7 +81,7 @@ class TestReviewAddView(TestCase):
         """Rating should not be more than 5."""
         review = ReviewFactory(rating=6)
         response = self.client.post(
-            path=reverse("api:review-add", kwargs={"user": self.responder.id}),
+            path=reverse("api:review-add", kwargs={"user": self.reviewee.id}),
             data={"text_body": review.text_body, "rating": review.rating},
         )
         self.assertEqual(response.status_code, 400)
@@ -74,7 +89,7 @@ class TestReviewAddView(TestCase):
     def test_post_method_create_review_wrong_text_empty(self):
         """Text of the review should not be empty."""
         response = self.client.post(
-            path=reverse("api:review-add", kwargs={"user": self.responder.id}),
+            path=reverse("api:review-add", kwargs={"user": self.reviewee.id}),
             data={"text_body": "", "rating": 2},
         )
         self.assertEqual(response.status_code, 400)
@@ -82,7 +97,7 @@ class TestReviewAddView(TestCase):
     def test_post_method_create_review_wrong_text_max(self):
         """Text of the review should not exceed 500 characters."""
         response = self.client.post(
-            path=reverse("api:review-add", kwargs={"user": self.responder.id}),
+            path=reverse("api:review-add", kwargs={"user": self.reviewee.id}),
             data={"text_body": ("&" * 501), "rating": 2},
         )
         self.assertEqual(response.status_code, 400)
@@ -90,7 +105,7 @@ class TestReviewAddView(TestCase):
     def test_post_method_create_review_cant_review_yourself(self):
         """Users should not be able to review themselves."""
         response = self.client.post(
-            path=reverse("api:review-add", kwargs={"user": self.user.id}),
+            path=reverse("api:review-add", kwargs={"user": self.reviewer.id}),
             data={"text_body": "I am the best!", "rating": 2},
         )
         self.assertEqual(response.status_code, 400)
