df70390f2f7eeb4023fa095e6246b3e745e2b962
YaroslavBorysko <yaroslavborysko@gmail.com>
2022-06-08 15:35:59 +0300

    edited serializer and views

--- beauty/api/urls.py
+++ beauty/api/urls.py
@@ -7,7 +7,7 @@ from api.views.order_views import (OrderApprovingView, OrderListCreateView,
 
 from api.views.review_views import (ReviewDisplayView, ReviewDisplayDetailView)
 
-from .views_api import (AllServicesListView, BusinessesListCreateAPIView,
+from .views_api import (AllServicesListCreateView, BusinessesListCreateAPIView,
                         BusinessDetailRUDView, CustomUserDetailRUDView,
                         CustomUserListCreateView, PositionListCreateView,
                         ReviewAddView, ServiceUpdateView)
@@ -76,8 +76,8 @@ urlpatterns = [
     ),
     path(
         "services/",
-        AllServicesListView.as_view(),
-        name="service-list",
+        AllServicesListCreateView.as_view(),
+        name="service-list-create",
     ),
     path("service/<int:pk>/",
          ServiceUpdateView.as_view(),
