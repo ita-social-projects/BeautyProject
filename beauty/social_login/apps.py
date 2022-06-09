fbd0e597e3f50fc76217488e2541aa83dd2aecf0
shevchenkoira <shevchenkoari@gmail.com>
2022-06-09 10:33:08 +0300

    #127: added blank line

--- beauty/social_login/apps.py
+++ beauty/social_login/apps.py
@@ -5,5 +5,6 @@ from django.apps import AppConfig
 
 class SocialLoginConfig(AppConfig):
     """App configuration for Google authorization."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "social_login"
