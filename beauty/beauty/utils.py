6e3d4ac24097f74581d2f5f980b43bfb77289ac3
Misha86 <mpolishchuk86@gmail.com>
2022-06-22 00:08:34 +0300

    feature/#252: Added custom_exception_handler to the utils.py

--- beauty/beauty/utils.py
+++ beauty/beauty/utils.py
@@ -113,3 +113,13 @@ def order_approve_decline_urls(order: object, approve_name="url_for_approve",
     urls[decline_name] = reverse("api:order-approving",
                                  kwargs=url_declined_params, request=request)
     return urls
+
+
+def custom_exception_handler(exc, context):
+    """Custom exception handler."""
+    from rest_framework.views import exception_handler
+    response = exception_handler(exc, context)
+    if response is not None:
+        response.data["status_code"] = response.status_code
+
+    return response
