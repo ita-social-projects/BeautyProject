b83d85171c43358793534c62b89366d9ea564508
Misha86 <mpolishchuk86@gmail.com>
2022-06-29 00:22:52 +0300

    feature/#326: Added smtplib validation to the change_order_status_to_decline

--- beauty/api/tasks.py
+++ beauty/api/tasks.py
@@ -1,4 +1,5 @@
 """Module with a celery tasks."""
+import smtplib
 
 from beauty.celery import app
 from api.models import Order
@@ -10,13 +11,14 @@ from beauty.utils import AutoDeclineOrderEmail
 logger = get_task_logger(__name__)
 
 
-@app.task
-def change_order_status_to_decline(order_id, site_name):
+@app.task(bind=True, default_retry_delay=5 * 60)
+def change_order_status_to_decline(self, order_id, site_name):
     """Change new order status to the declined.
 
      If a specialist did not approve or decline an order during 3 hours it declines automatic.
 
     Args:
+        self: current object
         order_id: order id
         site_name: site URL
     """
@@ -33,5 +35,7 @@ def change_order_status_to_decline(order_id, site_name):
             logger.info(f"{order} was declined")
     except Order.DoesNotExist:
         logger.info(f"Order with id={order_id} does not exist")
+    except smtplib.SMTPException as ex:
+        self.retry(exc=ex, countdown=5)
     except Exception as exc:
         logger.info(f"Error: {exc.args[0]}")
