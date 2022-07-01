a5cbbd842531f934914b0ed67955fca63bec2439
Misha86 <mpolishchuk86@gmail.com>
2022-07-01 08:56:29 +0300

    feature/#322: Added ssl.CERT_NONE to the app in the celery.py

--- beauty/beauty/celery.py
+++ beauty/beauty/celery.py
@@ -1,13 +1,19 @@
 """Module for Celery app configuration."""
 
 import os
+import ssl
 from celery import Celery
 from django.conf import settings
 
-
 # set the default Django settings module for the "celery" program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beauty.settings")
-app = Celery("beauty")
+app = Celery("beauty",
+             broker_use_ssl={
+                 "ssl_cert_reqs": ssl.CERT_NONE,
+             },
+             redis_backend_use_ssl={
+                 "ssl_cert_reqs": ssl.CERT_NONE,
+             })
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
