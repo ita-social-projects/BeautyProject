9ab7e640f929c75e666b507a27786e78e3d358d6
Misha86 <mpolishchuk86@gmail.com>
2022-07-05 15:18:35 +0300

    hotfix/#252: Added try_except decorator for order celery tasks

--- beauty/api/tasks.py
+++ beauty/api/tasks.py
@@ -3,6 +3,7 @@
 import logging
 import smtplib
 from beauty.celery import app
+from functools import wraps
 from api.models import Order
 from beauty.utils import (AutoDeclineOrderEmail, RemindAboutOrderEmail, ApprovingOrderEmail)
 
@@ -10,7 +11,38 @@ from beauty.utils import (AutoDeclineOrderEmail, RemindAboutOrderEmail, Approvin
 logger = logging.getLogger(__name__)
 
 
+def try_except(func):
+    """Return a decorator that checks function.
+
+    Args:
+        func (function): task function
+
+    Return:
+        inner (function): wrapped function
+    """
+    @wraps(func)
+    def inner(*args, **kwargs):
+        """Wrapper for task functions with try-except block.
+
+        Args:
+            *args: func position arguments
+            **kwargs: func keyword arguments
+        """
+        try:
+            func(*args, **kwargs)
+        except Order.DoesNotExist:
+            logger.info(f"Order with id={args[1]} does not exist")
+        except smtplib.SMTPException as ex:
+            logger.info(f"SMTPException: {ex.args[0]}")
+            args[0].retry(exc=ex, countdown=5)
+        except Exception as ex:
+            logger.info(f"Error: {ex.args[0]}")
+            args[0].retry(exc=ex, countdown=5)
+    return inner
+
+
 @app.task(bind=True, default_retry_delay=5 * 60)
+@try_except
 def change_order_status_to_decline(self, order_id, site_name):
     """Change new order status to the declined.
 
@@ -21,29 +53,19 @@ def change_order_status_to_decline(self, order_id, site_name):
         order_id: order id
         site_name: site URL
     """
-    try:
-        order = Order.objects.get(id=order_id)
-        if order.status == Order.StatusChoices.ACTIVE:
-            order.status = Order.StatusChoices.DECLINED
-            order.save()
-            context = {"order": order, "site_name": site_name}
+    order = Order.objects.get(id=order_id)
+    if order.status == Order.StatusChoices.ACTIVE:
+        order.status = Order.StatusChoices.DECLINED
+        order.save()
+        context = {"order": order, "site_name": site_name}
 
-            AutoDeclineOrderEmail(context=context).send([order.customer.email,
-                                                         order.specialist.email])
-
-            logger.info(f"{order} was declined")
-
-    except Order.DoesNotExist:
-        logger.info(f"Order with id={order_id} does not exist")
-    except smtplib.SMTPException as ex:
-        logger.info(f"SMTPException: {ex.args[0]}")
-        self.retry(exc=ex, countdown=5)
-    except Exception as ex:
-        logger.info(f"Error: {ex.args[0]}")
-        self.retry(exc=ex, countdown=5)
+        AutoDeclineOrderEmail(context=context).send([order.customer.email,
+                                                     order.specialist.email])
+        logger.info(f"{order} was declined")
 
 
 @app.task(bind=True, default_retry_delay=10 * 60)
+@try_except
 def reminder_for_customer(self, order_id, site_name):
     """Remind a customer about an order.
 
@@ -52,26 +74,17 @@ def reminder_for_customer(self, order_id, site_name):
         order_id: order id
         site_name: site URL
     """
-    try:
-        order = Order.objects.get(id=order_id)
-        if order.status == Order.StatusChoices.APPROVED:
-            context = {"order": order, "site_name": site_name}
+    order = Order.objects.get(id=order_id)
+    if order.status == Order.StatusChoices.APPROVED:
+        context = {"order": order, "site_name": site_name}
 
-            RemindAboutOrderEmail(context=context).send([order.customer.email])
+        RemindAboutOrderEmail(context=context).send([order.customer.email])
 
-            logger.info(f"{order} reminding was sent to the {order.customer.get_full_name()}")
-
-    except Order.DoesNotExist:
-        logger.info(f"Order with id={order_id} does not exist")
-    except smtplib.SMTPException as ex:
-        logger.info(f"SMTPException: {ex.args[0]}")
-        self.retry(exc=ex, countdown=5)
-    except Exception as ex:
-        logger.info(f"Error: {ex.args[0]}")
-        self.retry(exc=ex, countdown=5)
+        logger.info(f"{order} reminding was sent to the {order.customer.get_full_name()}")
 
 
 @app.task(bind=True, default_retry_delay=10 * 60)
+@try_except
 def send_message_for_specialist_consideration(self, order_id, site_name, is_secure):
     """Send approving order email.
 
@@ -81,25 +94,15 @@ def send_message_for_specialist_consideration(self, order_id, site_name, is_secu
         site_name (str): site URL
         is_secure (bool): check connection protocol
     """
-    try:
-        order = Order.objects.get(id=order_id)
+    order = Order.objects.get(id=1000)
 
-        context = {"order": order,
-                   "protocol": "https" if is_secure else "http",
-                   "domain": site_name,
-                   "site_name": site_name,
-                   }
+    context = {"order": order,
+               "protocol": "https" if is_secure else "http",
+               "domain": site_name,
+               "site_name": site_name,
+               }
 
-        ApprovingOrderEmail(context=context).send([order.specialist.email])
+    ApprovingOrderEmail(context=context).send([order.specialist.email])
 
-        logger.info(f"{order}: approving email was sent to the specialist "
-                    f"{order.specialist.get_full_name()}")
-
-    except Order.DoesNotExist:
-        logger.info(f"Order with id={order_id} does not exist")
-    except smtplib.SMTPException as ex:
-        logger.info(f"SMTPException: {ex.args[0]}")
-        self.retry(exc=ex, countdown=5)
-    except Exception as ex:
-        logger.info(f"Error: {ex.args[0]}")
-        self.retry(exc=ex, countdown=5)
+    logger.info(f"{order}: approving email was sent to the specialist "
+                f"{order.specialist.get_full_name()}")
