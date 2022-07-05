1f98f1fdebae1b98d6e89f497ee9fa1d73676f39
Misha86 <mpolishchuk86@gmail.com>
2022-07-05 14:40:28 +0300

    hotfix/#252: Added celery to sending consideration messages

--- beauty/api/tasks.py
+++ beauty/api/tasks.py
@@ -1,14 +1,13 @@
 """Module with a celery tasks."""
-import smtplib
 
+import logging
+import smtplib
 from beauty.celery import app
 from api.models import Order
-from celery.utils.log import get_task_logger
-
-from beauty.utils import (AutoDeclineOrderEmail, RemindAboutOrderEmail)
+from beauty.utils import (AutoDeclineOrderEmail, RemindAboutOrderEmail, ApprovingOrderEmail)
 
 
-logger = get_task_logger(__name__)
+logger = logging.getLogger(__name__)
 
 
 @app.task(bind=True, default_retry_delay=5 * 60)
@@ -33,12 +32,15 @@ def change_order_status_to_decline(self, order_id, site_name):
                                                          order.specialist.email])
 
             logger.info(f"{order} was declined")
+
     except Order.DoesNotExist:
         logger.info(f"Order with id={order_id} does not exist")
     except smtplib.SMTPException as ex:
+        logger.info(f"SMTPException: {ex.args[0]}")
         self.retry(exc=ex, countdown=5)
-    except Exception as exc:
-        logger.info(f"Error: {exc.args[0]}")
+    except Exception as ex:
+        logger.info(f"Error: {ex.args[0]}")
+        self.retry(exc=ex, countdown=5)
 
 
 @app.task(bind=True, default_retry_delay=10 * 60)
@@ -57,9 +59,47 @@ def reminder_for_customer(self, order_id, site_name):
 
             RemindAboutOrderEmail(context=context).send([order.customer.email])
 
+            logger.info(f"{order} reminding was sent to the {order.customer.get_full_name()}")
+
     except Order.DoesNotExist:
         logger.info(f"Order with id={order_id} does not exist")
     except smtplib.SMTPException as ex:
+        logger.info(f"SMTPException: {ex.args[0]}")
         self.retry(exc=ex, countdown=5)
-    except Exception as exc:
-        logger.info(f"Error: {exc.args[0]}")
+    except Exception as ex:
+        logger.info(f"Error: {ex.args[0]}")
+        self.retry(exc=ex, countdown=5)
+
+
+@app.task(bind=True, default_retry_delay=10 * 60)
+def send_message_for_specialist_consideration(self, order_id, site_name, is_secure):
+    """Send approving order email.
+
+    Args:
+        self (object): current object
+        order_id (int): order id
+        site_name (str): site URL
+        is_secure (bool): check connection protocol
+    """
+    try:
+        order = Order.objects.get(id=order_id)
+
+        context = {"order": order,
+                   "protocol": "https" if is_secure else "http",
+                   "domain": site_name,
+                   "site_name": site_name,
+                   }
+
+        ApprovingOrderEmail(context=context).send([order.specialist.email])
+
+        logger.info(f"{order}: approving email was sent to the specialist "
+                    f"{order.specialist.get_full_name()}")
+
+    except Order.DoesNotExist:
+        logger.info(f"Order with id={order_id} does not exist")
+    except smtplib.SMTPException as ex:
+        logger.info(f"SMTPException: {ex.args[0]}")
+        self.retry(exc=ex, countdown=5)
+    except Exception as ex:
+        logger.info(f"Error: {ex.args[0]}")
+        self.retry(exc=ex, countdown=5)
